0b385ff99887b97efa1e9e54ac326f81b263b164
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-09-25 16:56:44 +0100

    added test_game_can_play and .play function;
    when substituting string choice for integers 1-3, play function succesfully determines the winner.

--- app/models/game.py
+++ app/models/game.py
@@ -3,3 +3,15 @@ class Game:
     def __init__(self, player_1, player_2):
         self.player_1 = player_1
         self.player_2 = player_2
+
+
+    def play(self):
+        diff = self.player_1.choice - self.player_2.choice
+        diff += 5
+        result = diff % 3
+        if result == 0:
+            return f"{self.player_1.name} is the winner with {self.player_1.choice}!"
+        elif result == 1:
+            return f"{self.player_2.name} is the winner with {self.player_2.choice}!"
+        else:
+            return "It's a draw!"
--- app/tests/test_game.py
+++ app/tests/test_game.py
@@ -6,9 +6,14 @@ class GameTest(unittest.TestCase):
     
     def setUp(self):
         self.game_1 = Game(Player("Hal", "Rock"), Player("Dave", "Scissors"))
+        self.game_2 = Game(Player("Rick", 1), Player("Morty", 3))
 
     def test_game_has_player_1(self):
         self.assertEqual("Hal", self.game_1.player_1.name)
 
     def test_game_has_player_2(self):
         self.assertEqual("Scissors", self.game_1.player_2.choice)
+
+
+    def test_game_can_play(self):
+        self.assertEqual("Rick is the winner with 1!", self.game_2.play())
