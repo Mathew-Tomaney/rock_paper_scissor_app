890965ced5b8f3903ee9234c71463e3db7dcafca
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-09-25 18:24:06 +0100

    tweaking Player class so i can re-use input later

--- app/models/player.py
+++ app/models/player.py
@@ -1,10 +1,11 @@
 class Player:
-    def __init__(self, name, choice):
+    def __init__(self, name, input_choice):
         self.name = name
+        self.input_choice = input_choice
         options = {
             "Rock" : 1,
             "Paper" : 2,
             "Scissors" : 3
         }
-        self.choice = options[choice]
+        self.choice = options[input_choice]
         
