dd73ad64e55ef7ce4b9cfa41053b4386d68075c7
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-09-27 11:32:53 +0100

    added more to my html structure, including a form to capture user inputs. re-worked my controller functions to accept input from form and display results. seems to be working

--- app/controller.py
+++ app/controller.py
@@ -1,10 +1,19 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from app.models.player import Player
 from app.models.game import Game
 
-@app.route('/<player_1>/<choice_1>/<player_2>/<choice_2>')
-def index(player_1, choice_1, player_2, choice_2):
+@app.route('/')
+def rules():
+    return render_template("index.html", title= "Rules")
+
+
+@app.route('/play', methods=['POST'])
+def play():
+    player_1 = request.form['player_1']
+    choice_1 = request.form['choice_1']
+    player_2 = request.form['player_2']
+    choice_2 = request.form['choice_2']
     result = Game(Player(player_1, choice_1), Player(player_2, choice_2)).play()
     
     return render_template("index.html", title="Rock Paper Scissors", game_result= result)
