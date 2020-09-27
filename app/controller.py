eddd251774bb9af9231acad3457bcdd0e338d8a5
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-09-27 12:18:19 +0100

    adding links structure and templates for multiple pages

--- app/controller.py
+++ app/controller.py
@@ -4,8 +4,20 @@ from app.models.player import Player
 from app.models.game import Game
 
 @app.route('/')
+def index():
+    return render_template("index.html", title= "Home")
+
+@app.route('/rules')
 def rules():
-    return render_template("index.html", title= "Rules")
+    return render_template("rules.html", title="Rules")
+
+@app.route('/2_players')
+def two_players():
+    return render_template("2_players.html", title="2-Player Game")
+
+@app.route('/1_player')
+def one_player():
+    return render_template("1_player.html", title="1-Player Game")
 
 
 @app.route('/play', methods=['POST'])
@@ -16,5 +28,5 @@ def play():
     choice_2 = request.form['choice_2']
     result = Game(Player(player_1, choice_1), Player(player_2, choice_2)).play()
     
-    return render_template("index.html", title="Rock Paper Scissors", game_result= result)
+    return render_template("results.html", title="Results", game_result= result)
 
--- app/models/game.py
+++ app/models/game.py
@@ -3,14 +3,17 @@ class Game:
     def __init__(self, player_1, player_2):
         self.player_1 = player_1
         self.player_2 = player_2
+        self.winning_player = None
 
 
     def play(self):
         result = ((self.player_1.choice - self.player_2.choice) + 5 ) % 3 
         
         if result == 0:
+            self.winning_player = self.player_1
             return f"{self.player_1.name} is the winner with {self.player_1.input_choice}!"
         elif result == 1:
+            self.winning_player = self.player_2
             return f"{self.player_2.name} is the winner with {self.player_2.input_choice}!"
         else:
             return "It's a draw!"
